82f61a2bfffaa8fdaf56328bffdc687e80f0e405
Mikael Lindlof <mikuz.dev@gmail.com>
2012-05-11 22:48:26 +0300

    Instead of activity only context is needed to resize icon.

--- src/fi/mikuz/boarder/util/IconUtils.java
+++ src/fi/mikuz/boarder/util/IconUtils.java
@@ -1,24 +1,24 @@
 package fi.mikuz.boarder.util;
 
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.util.DisplayMetrics;
+import android.util.Log;
 
 /**
  * 
  * @author Jan Mikael Lindlöf
  */
 public class IconUtils {
-	public static Bitmap resizeIcon(Activity activity, Bitmap bitmap, float pixelsPerInch) {
-		DisplayMetrics metrics = new DisplayMetrics();
-		activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+	public static Bitmap resizeIcon(Context context, Bitmap bitmap, float pixelsPerInch) {
+		DisplayMetrics metrics = context.getResources().getDisplayMetrics();
 		
 		float density = metrics.densityDpi;
 		float imageSize = density/pixelsPerInch;
 		
-        int width = bitmap.getWidth();
-        int height = bitmap.getHeight();
+		int width = bitmap.getWidth();
+		int height = bitmap.getHeight();
         float scaleWidth = ((float) imageSize) / width;
         float scaleHeight = ((float) imageSize) / height;
        
