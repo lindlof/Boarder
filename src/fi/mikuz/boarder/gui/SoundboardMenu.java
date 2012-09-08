lindlof/Boarder

commit aed7ace8a0ba16449ca15165153d761e52a677a7

Board list updating preserves scroll state of the list.

src/fi/mikuz/boarder/gui/SoundboardMenu.java
@@ -90,6 +90,7 @@ public class SoundboardMenu extends BoarderListActivity {
 	private BoardsDbAdapter mDbHelper;
     private GlobalVariablesDbAdapter mGlobalVariableDbHelper;
 	private int mMoveBoard = -1;
+	private BoardsCursorAdapter mBoardsCursorAdapter;
 	
 	Intent mIntent;
     String mAction;
@@ -352,7 +353,12 @@ public class SoundboardMenu extends BoarderListActivity {
     
     private void refreshBoards() {
     	try {
-    		setListAdapter(new BoardsCursorAdapter(mDbHelper.fetchAllBoards()));
+    		if (mBoardsCursorAdapter == null) {
+    			mBoardsCursorAdapter = new BoardsCursorAdapter(mDbHelper.fetchAllBoards());
+        		setListAdapter(mBoardsCursorAdapter);
+    		} else {
+    			mBoardsCursorAdapter.changeCursor(mDbHelper.fetchAllBoards());
+    		}
     	} catch (IllegalStateException e) {
     		Log.w(TAG, "Unable to refresh board list", e);
     	}

commit afc801e017b9b0eebc4786cc52fe946c59e6543e

Fixed soundboard menu crash introduced in aed7ace8a0ba16449ca15165153d761e52a677a7

src/fi/mikuz/boarder/gui/SoundboardMenu.java
@@ -357,7 +357,7 @@ public class SoundboardMenu extends BoarderListActivity {
     			mBoardsCursorAdapter = new BoardsCursorAdapter(mDbHelper.fetchAllBoards());
         		setListAdapter(mBoardsCursorAdapter);
     		} else {
-    			mBoardsCursorAdapter.changeCursor(mDbHelper.fetchAllBoards());
+    			mBoardsCursorAdapter.swapCursor(mDbHelper.fetchAllBoards());
     		}
     	} catch (IllegalStateException e) {
     		Log.w(TAG, "Unable to refresh board list", e);
@@ -398,10 +398,7 @@ public class SoundboardMenu extends BoarderListActivity {
         }
 
         @Override
-        public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        	startManagingCursor(cursor);
-
-            Cursor c = getCursor();
+        public View newView(Context context, Cursor c, ViewGroup parent) {
             startManagingCursor(c);
 
             final LayoutInflater inflater = LayoutInflater.from(context);