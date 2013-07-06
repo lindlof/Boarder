3044d511f0e377e87481dcb69d27186c761ef412
Mikael Lindlöf <mikuz.dev@gmail.com>
2013-07-06 17:12:11 +0300

    Add lengthy explanation of the implicit collection hack.

--- src/fi/mikuz/boarder/component/soundboard/ImplicitSoundList.java
+++ src/fi/mikuz/boarder/component/soundboard/ImplicitSoundList.java
@@ -20,7 +20,24 @@
 package fi.mikuz.boarder.component.soundboard;
 
 import java.util.List;
+import fi.mikuz.boarder.util.XStreamUtil;
 
+/**
+ * XStream collection hack.
+ * <p>
+ * This collection is used in {@link GraphicalSoundboard} and declared as
+ * implicit collection in {@link XStreamUtil}.
+ * <p>
+ * Implicit collection class itself is ignored in serialized XML and all collection 
+ * items it contains are directly inserted without wrapping object.
+ * I want that wrapping object so an extra intermediary object was introduced.
+ * This is that extra object.
+ * <p>
+ * The need to use implicit collection comes from XStreams inability to use 
+ * custom collections without introducing a bogus class attribute.
+ * That class attribute could create serious problems with loading legacy
+ * boards in the future.
+ */
 public class ImplicitSoundList {
 	private GraphicalSoundList list;
 
